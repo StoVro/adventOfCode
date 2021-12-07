1e70eed0e1a7c75138df957d3628e5d8cc72f9bf
StoVro <95437564+StoVro@users.noreply.github.com>
2021-12-07 09:17:19 +0100

    added day 7

---
+++ AdventOfCode2021/src/main/kotlin/code/Day7.kt
@@ -0,0 +1,60 @@
+package code
+
+import input.INPUT_DAY7_HORIZONTAL_VALUES
+
+fun main() {
+    //day7FirstPuzzle()
+    day7SecondPuzzle()
+}
+
+fun day7FirstPuzzle() {
+    val minValue = INPUT_DAY7_HORIZONTAL_VALUES.minByOrNull { it }
+    val maxValue = INPUT_DAY7_HORIZONTAL_VALUES.maxByOrNull { it }
+
+    val range = minValue!!..maxValue!!
+
+    val groupedNumbers = INPUT_DAY7_HORIZONTAL_VALUES.groupingBy { it }.eachCount()
+    var minPosFuel : Pair<Int, Int> = Pair(0, 0)
+
+    range.forEachIndexed { index, currentRange ->
+        val fuel = groupedNumbers.map { it.key.getAbsoluteValue(currentRange) * it.value }.sum()
+        if(index == 0) {
+            minPosFuel = Pair(currentRange, fuel)
+        }
+        if( fuel < minPosFuel.second ) {
+            minPosFuel = Pair(currentRange, fuel)
+        }
+    }
+
+    println(minPosFuel)
+}
+
+fun day7SecondPuzzle() {
+    val minValue = INPUT_DAY7_HORIZONTAL_VALUES.minByOrNull { it }
+    val maxValue = INPUT_DAY7_HORIZONTAL_VALUES.maxByOrNull { it }
+
+    val range = minValue!!..maxValue!!
+
+    val groupedNumbers = INPUT_DAY7_HORIZONTAL_VALUES.groupingBy { it }.eachCount()
+    var minPosFuel : Pair<Int, Int> = Pair(0, 0)
+
+    range.forEachIndexed { index, currentRange ->
+        val fuel = groupedNumbers.map { getFuelForOneNumber(it.key, currentRange) * it.value }.sum()
+        if(index == 0) {
+            minPosFuel = Pair(currentRange, fuel)
+        }
+        if( fuel < minPosFuel.second ) {
+            minPosFuel = Pair(currentRange, fuel)
+        }
+    }
+
+    println(minPosFuel)
+}
+
+fun Int.getAbsoluteValue(otherValue: Int) : Int {
+    return if(this > otherValue) this - otherValue else otherValue - this
+}
+
+fun getFuelForOneNumber(key: Int, currentRange: Int) : Int {
+    return (1..key.getAbsoluteValue(currentRange)).sum()
+}
